8f387608b5e0a9e345934aaea0ab765078a9a16b
PhongNgo1776 <ngothanhphong1776@gmail.com>
2016-11-06 11:24:12 +0700

    new project

---
+++ src/main/java/com/fpt/erp/erp_root/App.java
@@ -0,0 +1,17 @@
+package com.fpt.erp.erp_root;
+
+import java.util.stream.Stream;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        Stream<Integer> stream = Stream.of(1, 2, 3, 4);
+        stream.forEach(System.out::println);
+    }
+}
