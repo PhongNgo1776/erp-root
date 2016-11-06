25305cbc844af756b58b278aab06a0b6d2b4852a
PhongNgo1776 <ngothanhphong1776@gmail.com>
2016-11-06 11:29:39 +0700

    update

--- src/main/java/com/fpt/erp/erp_root/App.java
+++ src/main/java/com/fpt/erp/erp_root/App.java
@@ -11,7 +11,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        Stream<Integer> stream = Stream.of(1, 2, 3, 4);
+        Stream<Integer> stream = Stream.of(1, 2, 3);
         stream.forEach(System.out::println);
     }
 }
